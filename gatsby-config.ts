d13e45f4ced714c297244d33299f49e1ef91a55e
Let Godoy <let.godoy@live.com>
2023-02-20 12:57:14 -0300

    gastby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -30,12 +30,12 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#0F2B76`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        // theme_color: `#0F2B76`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Logo.png`, // This path is relative to the root of the site.
       },
     },
   ],
